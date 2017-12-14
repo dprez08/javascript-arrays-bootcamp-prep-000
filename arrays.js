8c1bb3762121413a844390c64d49c8f10c38934d
David Perez <davperez89@gmail.com>
2017-12-14 06:06:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,7 +25,7 @@ function accessElementInArray(array, index){
 
 function destructivelyRemoveElementFromBeginningOfArray(array){
   array.shift();
-  array;
+  return array;
 }
 
 function removeElementFromBeginningOfArray(array, element){
