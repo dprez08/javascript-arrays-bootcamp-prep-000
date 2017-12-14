cb1d9178764b8ecfa34195b53e6d2c985faf7021
David Perez <davperez89@gmail.com>
2017-12-14 05:59:01 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,41 @@
+var chocolateBars = ["snickers", "hundred grand", "kit kat","skittles"];//
+
+
+function addElementToBeginningOfArray(array,element){
+  return [element,...array];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element);
+  return array ;
+}
+
+function addElementToEndOfArray(array, element){
+  return [...array,element];
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element);
+    return array;
+}
+
+function accessElementInArray(array, index){
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift();
+  array;
+}
+
+function removeElementFromBeginningOfArray(array, element){
+  return array.slice(2);
+}
+function destructivelyRemoveElementFromEndOfArray(array,element){
+  array.pop(element);
+}
+
+function removeElementFromEndOfArray(array, element){
+  return array.slice(0, array.length - 1);
+}
+
